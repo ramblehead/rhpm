b0ad275c823b4db1a8bdbc75c997aeb814072980
ramblehead <v.rybynok@gmail.com>
2023-03-06 00:06:54 +0000

    Checkpoint

---
+++ packages/shrinkpdf/shrinkpdf-ImageMagick.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+convert -density 100x100 -quality 60 -compress jpeg \
+        E_Rybynok_IELTS_Life_Skills_A2-orig.pdf \
+        E_Rybynok_IELTS_Life_Skills_A2-100.pdf
---
+++ packages/shrinkpdf/shrinkpdf-ps2pdf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ps2pdf -dPDFSETTINGS=/printer \
+       E_Rybynok_IELTS_Life_Skills_A2-orig.pdf \
+       E_Rybynok_IELTS_Life_Skills_A2-ebook.pdf
