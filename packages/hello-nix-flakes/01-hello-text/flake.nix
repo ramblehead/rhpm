1f8cd97d3f94b3eb5a9d87db620a4498ac888e63
ramblehead <v.rybynok@gmail.com>
2024-07-07 01:13:28 +0100

    Checkpoint

---
+++ packages/hello-nix-flakes/01-hello-text/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) stdenv;
+      in {
+        # nix build .#hello
+        packages.hello = stdenv.mkDerivation {
+          name = "hello-rh-tiny-v0";
+          src = ./.;
+          installPhase = ''
+            echo "Hello, world!" > $out
+          '';
+        };
+
+        # nix build
+        defaultPackage = self.packages.${system}.hello;
+      });
+}
